b2eef49cda1d5eb9157c9034ba8be7d40cd38272
kaushikmdeo <kaushikmdeo@gmail.com>
2020-08-31 06:10:39 +0530

    added iniital movie fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [moviesPerPage, setMoviesPerPage] = useState(10);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            setLoading(true);
+            const movies = await axios.get('http://localhost:5000/api/movies?page=1&limit=10&orderBy=1&filter=s');
+            setMovies(movies.data.allMovies);
+            setLoading(false);
+        };
+        fetchMovies();
+    }, [])
+    console.log('movies', movies);
+    const renderMovies = () => {
+        return movies.map(movie => {
+            return (
+                <div>
+                    <h3>{movie.name}</h3>
+                </div>
+            )
+        })
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <h1>Movies List</h1>
+        </header>
     </div>
   );
 }
